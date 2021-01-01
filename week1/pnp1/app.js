03377fd6349d1e45477e3158e1becaf177c26b0d
kara temple <you@example.com>
2020-12-31 21:47:12 -0400

    week2

--- week1/pnp1/app.js
+++
@@ -1,4 +0,0 @@
-const {add, subtract, multiply, divide} = require("./math")
-
-console.log(add(5, 6))
-console.log(subtract(28, 12))
---
+++ week2/project/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+
+const users = [
+    {name: "john", age: 65},
+    {name: "mary", age: 45},
+    {name: "steve", age: 37},
+    {name: "mike", age: 49}
+
+]
+
+const sailboats = [
+    {name: "bavaria"},
+    {name: "beneteau"},
+    {name: "oyster"},
+    {name: "hylas"}
+
+]
+
+const powerboats = [
+    {name: "yellowfin"},
+    {name: "boston whaler"},
+    {name: "donzi"},
+    {name: "contender"}
+]
+
+app.get("/users", (req, res) => {
+    res.send(users);
+})
+
+app.get("/sailboats", (req, res) => {
+    res.send(sailboats);
+})
+
+app.get("/powerboats", (req, res) => {
+    res.send(powerboats);
+})
+
+app.listen(8000, () => {
+    console.log("the server is running on 8000")
+} )
