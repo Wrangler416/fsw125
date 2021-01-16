fdc672528a39b4cd8c7a20a4c5d5d66e003736fc
kara temple <you@example.com>
2021-01-16 15:29:59 -0400

    week4bounty

---
+++ week4/pnp/bountyRouter.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const bountyRouter = express.Router()
+const { v4: uuidv4 } = require('uuid')
+
+
+const bounties = [
+    
+    {fName: "Kanan", lName: "Jaris", isAlive: true, number: 10, type: "Jedi", _id: uuidv4()},
+    {fName: "Raynar", lName: "Thul", isAlive: true, number: 20, type: "Jedi", _id: uuidv4()},
+    {fName: "Tenel", lName: "Ka", isAlive: true, number: 50, type: "Jedi", _id: uuidv4()},
+    {fName: "Anakin", lName: "Skywalker", isAlive: true, number: 100, type: "Jedi", _id: uuidv4()}
+]
+
+bountyRouter.route("/")
+
+    .get((req, res) => {
+        res.send(bounties)
+    })
+
+    .get("/:bountyId", (req, res) => {
+        const bountyId = req.params.bountyId
+        const foundBounty = bounties.find(bounty => bounty._id === bountyId)
+        res.send(foundBounty)
+    })
+
+    .post((req, res) => {
+        const newBounty = req.body
+        newBounty._id = uuidv4()
+        bounties.push(newBounty)
+        res.send(`You added ${newBounty.fName } to the database!`)
+    })
+
+    .delete("/:bountyId", (req, res) => {
+        const bountyId = req.params.bountyId
+        const bountyIndex = bounties.findIndex(bounty => bounty._id === bountyId)
+        bounties.splice(bountyIndex, 1)
+        res.send("Yay, deleted ")
+    })
+
+   
+module.exports = bountyRouter
